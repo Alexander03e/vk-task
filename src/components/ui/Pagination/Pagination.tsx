396da81fbad405f91788279919bda1ffe6d18e61
Alexander03e <cfifeg1@gmail.com>
2024-04-29 23:26:14 +0300

    feat: remake pagination

--- src/components/ui/Pagination/Pagination.tsx
+++ src/components/ui/Pagination/Pagination.tsx
@@ -1,14 +1,16 @@
-import { memo, useMemo, useState } from "react";
+import { useState } from "react";
 import styles from "./Pagination.module.scss";
 
 export const Pagination = ({
   page,
   totalPages,
   onPageChange,
+  pageCount = 5,
 }: {
   page: number;
   totalPages: number;
   onPageChange: (val: number) => void;
+  pageCount?: number;
 }) => {
   const [activePage, setActivePage] = useState(page);
 
@@ -25,17 +27,7 @@ export const Pagination = ({
       onPageChange(activePage - 1);
     }
   };
-  const pageNumbers = useMemo(() => {
-    const numbers = [];
-    const startPage = Math.max(activePage - 1, 1);
-    const endPage = Math.min(activePage + 3, totalPages);
 
-    for (let i = startPage; i <= endPage; i++) {
-      numbers.push(i);
-    }
-
-    return numbers;
-  }, [activePage, totalPages]);
   const handlePageChange = (pageNumber: number) => {
     setActivePage(pageNumber);
     onPageChange(pageNumber);
@@ -64,57 +56,62 @@ export const Pagination = ({
       ) : (
         ""
       )}
-      {pageNumbers.map((pageNumber) => (
-        <PageItem
-          key={pageNumber}
-          pageNumber={pageNumber}
-          activePage={activePage}
-          onPageChange={handlePageChange}
-        />
-      ))}
-      <button className={styles.paginateButton} onClick={nextPage}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-          />
-        </svg>
-      </button>
+      {Array.from({ length: pageCount }, (_, i) => {
+        const curPage = activePage + i;
+        if (curPage <= totalPages) {
+          return (
+            <PaginationItem
+              activePage={activePage}
+              curPage={curPage}
+              handlePageChange={handlePageChange}
+              index={i}
+            />
+          );
+        }
+      })}
+      {activePage < totalPages ? (
+        <button className={styles.paginateButton} onClick={nextPage}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+            />
+          </svg>
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
 
-const PageItem = memo(
-  ({
-    pageNumber,
-    activePage,
-    onPageChange,
-  }: {
-    pageNumber: number;
-    activePage: number;
-    onPageChange: (val: number) => void;
-  }) => {
-    const handlePageChange = () => {
-      onPageChange(pageNumber);
-    };
-
-    return (
-      <button
-        className={`${styles.paginateButton} ${
-          activePage === pageNumber ? styles.active : ""
-        }`}
-        onClick={handlePageChange}
-      >
-        {pageNumber}
-      </button>
-    );
-  }
-);
+const PaginationItem = ({
+  activePage,
+  curPage,
+  handlePageChange,
+  index,
+}: {
+  activePage: number;
+  curPage: number;
+  index: number;
+  handlePageChange: (val: number) => void;
+}) => {
+  return (
+    <button
+      className={`${styles.paginateButton} ${
+        activePage === curPage ? styles.active : ""
+      }`}
+      onClick={() => handlePageChange(curPage)}
+    >
+      {activePage + index}
+    </button>
+  );
+};
